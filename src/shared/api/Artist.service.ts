9fb605cf64d3a065b7b423fdba377bf883f41f38
Владислав Аткишкин <atkishkinvlad@yandex.ru>
2025-08-20 23:19:44 +0500

    chore(api): create artist service

--- src/shared/api/Artist.service.ts
+++ src/shared/api/Artist.service.ts
@@ -2,21 +2,24 @@ import type { paths } from "./schema";
 import type { Client } from "openapi-fetch";
 
 export class Artist {
-    constructor(private client: Client<paths>, private appId: string) {
-        this.client = client;
-        this.appId = appId;
-    }
-    
-    getArtist(artistName: string) {
-        return this.client.GET("/artists/{artistname}", {
-            params: {
-                path: {
-                    artistname: artistName
-                },
-                query: {
-                    app_id: this.appId
-                },
-            }
-        })
-    }
+  constructor(
+    private client: Client<paths>,
+    private appId: string,
+  ) {
+    this.client = client;
+    this.appId = appId;
+  }
+
+  getArtist(artistName: string) {
+    return this.client.GET("/artists/{artistname}", {
+      params: {
+        path: {
+          artistname: artistName,
+        },
+        query: {
+          app_id: this.appId,
+        },
+      },
+    });
+  }
 }
