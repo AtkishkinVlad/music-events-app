230e0006ddffdcad5e9bbea262e69efc5dbf72d6
Владислав Аткишкин <atkishkinvlad@yandex.ru>
2025-08-20 22:57:06 +0500

    chore(fsd-linter): add steiger for lint fsd rules

--- steiger.config.ts
+++ steiger.config.ts
@@ -1,12 +1,12 @@
-import { defineConfig } from 'steiger'
-import fsd from '@feature-sliced/steiger-plugin'
+import { defineConfig } from "steiger";
+import fsd from "@feature-sliced/steiger-plugin";
 
 export default defineConfig([
   ...fsd.configs.recommended,
   {
-    files: ['./src/shared/**'],
+    files: ["./src/shared/**"],
     rules: {
-      'fsd/public-api': 'off',
+      "fsd/public-api": "off",
     },
   },
-])
+]);
